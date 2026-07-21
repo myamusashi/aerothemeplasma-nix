a1769a84abbb8bda801b89080cc7500362a5ef86
myamusashi <myamusashi@proton.me>
2026-07-21 10:50:26 +0700

    fix dependency

--- pkgs/aeroshell/kwin/default-rules.nix
+++ pkgs/aeroshell/kwin/default-rules.nix
@@ -2,21 +2,35 @@
   stdenv,
   aeroshell-kwin-repo,
   kdePackages,
-  cmake
+  cmake,
+  lib
 }:
 stdenv.mkDerivation {
   name = "aeroshell-default-rules";
   version = "2026-02-22";
   src = aeroshell-kwin-repo;
-  
+
   postPatch = ''
     substituteInPlace CMakeLists.txt --replace-fail \
       "add_subdirectory(effects_cpp)" "add_subdirectory(rules)"
+    substituteInPlace CMakeLists.txt --replace-fail \
+      "find_package(KDecoration3 REQUIRED)" ""
+    sed -i \
+      -e '/^if(KWIN_BUILD_WAYLAND)/,/^endif()/d' \
+      -e '/find_package(KWinX11/d' \
+      -e '/find_package(KWinDBusInterface/d' \
+      -e '/find_package(epoxy/d' \
+      -e '/find_package(Vulkan/d' \
+      -e '/find_package(WaylandProtocols/,/^)/d' \
+      CMakeLists.txt
   '';
-  
-  buildInputs = with kdePackages; [ 
-    extra-cmake-modules qtdeclarative 
+
+  buildInputs = with kdePackages; [
+    extra-cmake-modules qtdeclarative
     qttools kconfig ki18n
+    kconfigwidgets kcoreaddons kcrash kio
+    kservice knotifications kwidgetsaddons
+    kwindowsystem kguiaddons kcmutils ksvg
   ];
   nativeBuildInputs = [ cmake kdePackages.wrapQtAppsHook ];
   cmakeFlags = [ "-DKWIN_INSTALL_MISC=false" ];
--- pkgs/external/fonts/lucida-console.nix
+++ pkgs/external/fonts/lucida-console.nix
@@ -1,19 +1,14 @@
 {
   stdenvNoCC,
-  requireFile
+  fetchurl
 }:
 stdenvNoCC.mkDerivation {
   pname = "lucida-console";
   version = "5";
 
-  src = requireFile rec {
-    name = "lucon.ttf";
-    hash = "sha256-9N9NbRpp0k4I2irS9FIDtMcUfdqlGHzNcwh5A8o20Xc=";
-    message = ''
-      Please grab C:\Windows\Fonts\${name} from an up-to-date install of Windows 7.
-      Once you have grabbed the file, run "nix store add-file ${name}", then try again.
-      See https://github.com/nyakase/aerothemeplasma-nix#lucida-console-font for more info.
-    '';
+  src = fetchurl {
+    url = "https://raw.githubusercontent.com/FSKiller/Microsoft-Fonts/main/lucon.ttf";
+    hash = "sha256-LKLP3iY/pCDo05Qg8HXumsgLwHlCcsU6xFcaiPigdaY=";
   };
 
   dontUnpack = true;
